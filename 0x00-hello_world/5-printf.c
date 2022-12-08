2eb3dc6a17de8dcddaa5c3f49df96f01857ca550
Sampul-CodeMine <studysampul@gmail.com>
2022-12-08 13:49:48 +0000

    C program to output using the printf function or method.

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -9,6 +9,7 @@ int main(void)
 {
 	/* This function prints out text using the printf function */
 
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	char my_char[] = "with proper grammar, but the outcome is a piece of art,\n";
+	printf("%s", my_char);
 	return (0);
 }
