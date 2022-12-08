bb39a9200ea24df03cb5235cd68e28af597a37b6
Sampul-CodeMine <studysampul@gmail.com>
2022-12-08 13:06:50 +0000

    Added comments and indentations to my program file.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
+/**
+ * main: Entry point
+ *
+ * Return 0: Success 
+ */
+
 int main(void)
 {
-	char my_char[] = "\"Programming is like building a multilingual puzzle\n";
+	/* declaration of variable my_char too hold my  string data */
+
+	char my_char[] = "\"Programming is like building a multilingual puzzle";
+	
 	puts(my_char);
+	
 	return 0;
 }
