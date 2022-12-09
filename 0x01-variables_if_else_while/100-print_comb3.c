c5ed6e823c2fc19d7929564085e3ed710abe170a
Sampul-CodeMine <studysampul@gmail.com>
2022-12-10 00:51:11 +0100

    C program to print a combination of 2-digit numbers tht appear differently.

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -11,13 +11,13 @@ int main(void)
 
 	int iter1, iter2;
 
-	for (iter1 = 0; iter1 < 10; iter1++)
+	for (iter1 = 0; iter1 < 9; iter1++)
 	{
-		for (iter2 = 0; iter2 < 10; iter2++)
+		for (iter2 = (iter1 + 1); iter2 < 10; iter2++)
 		{
 			putchar((iter1 % 10) + '0');
 			putchar((iter2 % 10) + '0');
-			if (iter1 == 9 && iter2 == 9)
+			if (iter1 == 8 && iter2 == 9)
 				continue;
 			putchar(',');
 			putchar(' ');
