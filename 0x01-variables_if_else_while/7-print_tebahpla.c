aafc34a73f02403e6c4f77f5c202eac14c5d4569
Sampul-CodeMine <studysampul@gmail.com>
2022-12-09 16:43:04 +0000

    A C program to print the alphabet in reverse form using putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	/* Program to print alphabets from z- a using putchar */
+
+	int alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
