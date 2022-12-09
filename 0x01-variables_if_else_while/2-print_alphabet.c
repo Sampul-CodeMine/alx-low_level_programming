1b443e15042e86965cc7973db19a96bc8da0a4a9
Sampul-CodeMine <studysampul@gmail.com>
2022-12-09 13:38:26 +0000

    C program to print the letters of the alphabets in small letters.

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -10,6 +10,7 @@ int main(void)
 	/* Program to print alphabets from a - z in small caps using putchar */
 
 	int alphabet;
+
 	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 		putchar(alphabet);
 	printf("\n");
