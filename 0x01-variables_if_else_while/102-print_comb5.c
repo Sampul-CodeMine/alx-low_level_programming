92f15c6faec7ae0ee1221d14ebac48888ee6ee3b
Sampul-CodeMine <studysampul@gmail.com>
2022-12-10 01:35:54 +0100

    C program to print all possible combination of 2 digits numbers

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+  * main - Entry point
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	/* Program to print all possible combo of 2-digit numbers  */
+
+	int iter1, iter2;
+
+	for (iter1 = 0; iter1 < 99; iter1++)
+	{
+		for (iter2 = (iter1 + 1); iter2 < 10; iter2++)
+		{
+			putchar((iter1 / 10) + '0');
+			putchar((iter1 % 10) + '0');
+			putchar(' ');
+			putchar((iter2 / 10) + '0');
+			putchar((iter2 % 10) + '0');
+			if (iter1 == 98 && iter2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
