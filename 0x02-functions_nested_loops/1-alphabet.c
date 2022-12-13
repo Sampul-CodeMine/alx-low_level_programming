0f6b9b6a0e53cf9b158c699b5557425b7ad54f84
Sampul-CodeMine <studysampul@gmail.com>
2022-12-13 11:35:55 +0000

    Updated my script to adhere to Betty coding style.

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /**
-  * print_the_alphabets - This function prints the English alphabets
+  * print_alphabet - This function prints the English alphabets
   * Return: Empty/Nothing
   */
 void print_alphabet(void)
 {
 	char chr;
+
 	for (chr = 'a'; chr <= 'z'; chr++)
 	{
 		_putchar(chr);
