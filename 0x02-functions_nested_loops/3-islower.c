16d601198b58ab4dcd928ef04bfa9c841e6b483e
Sampul-CodeMine <studysampul@gmail.com>
2022-12-13 12:06:10 +0000

    A C program to check if an alphabet is upper cased or lower cased

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,14 @@
 
 /**
   * _islower - Function to check if a character is lower cased or not
+  * @chr: alphabet to test if uppercased or lowercased
   * Return: 1 if true or 0 if false
   */
 
 int _islower(int chr)
 {
 	if (chr >= 'a' && chr <= 'x')
-		return 1;
+		return (1);
 	else
-		return 0;
+		return (0);
 }
