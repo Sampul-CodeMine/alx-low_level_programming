051e43d3dbf90a671ec5ab9bc72bc87fc02d36d7
Sampul-CodeMine <studysampul@gmail.com>
2022-12-14 14:09:33 +0000

    Updated my times table code

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,40 +8,30 @@ void print_times_table(int num)
 {
 	int iter1, iter2, product = 0;
 
-	if (num == 0)
-		_putchar('0');
-	else if (num >= 1 && num <= 15)
+	if (num >= 0 && num <= 15)
 	{
 		for (iter1 = 0; iter1 <= num; iter1++)
 		{
-			for (iter2 = 0; iter2 <= num; iter2++)
+			_putchar('0');
+			for (iter2 = 1; iter2 <= num; iter2++)
 			{
 				product = iter1 *  iter2;
-				if (iter2 != 0)
-				{
-					_putchar(',');
+				_putchar(',');
+				_putchar(' ');
+				if (product <= 99)
 					_putchar(' ');
-				}
-				if (product < 10 && iter2 != 0)
-				{
+				if (product <= 9)
 					_putchar(' ');
-					_putchar(' ');
-					_putchar((product % 10) + '0');
-				}
-				else if (product >= 10 && product < 100)
-				{
-					_putchar(' ');
-					_putchar((product / 10) + '0');
-					_putchar((product % 10) + '0');
-				}
-				else if (product >= 100 && iter2 != 0)
+				if (product >= 100)
 				{
 					_putchar((product / 100) + '0');
 					_putchar(((product / 10) % 10) + '0');
-					_putchar((product % 10) + '0');
 				}
-				else
-					_putchar((product % 10) + '0');
+				else if (product <= 99 && product >= 10)
+				{
+					_putchar((product / 10) + '0');
+				}
+				_putchar((product % 10) + '0');
 			}
 			_putchar('\n');
 		}
---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+  * main - Entry point
+  * Return: Always 0 (success)
+  */
+
+int main(void)
+{
+	int iterator;
+	unsigned long first_val, second_val, container, total;
+
+	
+	first_val = 0;
+	second_val = 1;
+	total = 0;
+	for (iterator = 0; iterator < 50; iterator++)
+	{
+		container = first_val + second_val;
+		first_val = second_val;
+		second_val = container;
+		if (container % 2 == 0 && container < 4000000)
+			total += container;
+	}
+	printf("%lu\n", total);
+	return (0);
+}
