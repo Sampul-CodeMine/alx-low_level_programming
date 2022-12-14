b7be2cafd0861aed497045e62218d9f0a7ac3782
Sampul-CodeMine <studysampul@gmail.com>
2022-12-14 09:02:10 +0000

    A C program to carry out addition - adding two numbers and returning the sum.

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * add - Function to add two numbers together
+  * Return: the value computed
+ */
+
+int add(int num_one, int num_two)
+{
+	int sum;
+
+	sum = (num_one + num_two);
+	return (sum);
+}
