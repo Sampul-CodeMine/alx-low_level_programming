15524f61bd7def7188d9ccdca4c808eb7dcd1d75
Sampul-CodeMine <studysampul@gmail.com>
2022-12-15 18:27:20 +0100

    Print a number preserving its value and signs

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * print_number - prints a number preserving its signs
+ * @num: the number to preserve and print
+ * Return: Nothing
+ */
+
+void print_number(int n)
+{
+	long len, res, i, temp, expo;
+
+	res = n;
+	expo = len =  1;
+	
+	/*Check if the number is negative */
+	if (res < 0)
+	{
+		res *= -1;
+		_putchar('-');
+	}
+
+	temp = res;
+	while (temp >= 10)
+	{
+		len++;
+		temp /= 10;
+	}
+
+	for (i = 1; i < len; i++)
+		expo *= 10;
+	
+	while (expo > 1)
+	{
+		_putchar((res / expo) % 10 + '0');
+		expo /= 10;
+	}
+	_putchar(res % 10 + '0');
+}
