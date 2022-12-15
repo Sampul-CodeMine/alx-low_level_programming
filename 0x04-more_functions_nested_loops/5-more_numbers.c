857300220642478255fc31740350b793d62f847a
Sampul-CodeMine <studysampul@gmail.com>
2022-12-15 14:18:29 +0100

    C program to display number 1 through 14 ten times.

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * more_numbers - function to repeatedly print 0 through 14 ten times
+ * Return: Nothing
+ */
+
+void more_numbers(void)
+{
+	int counts = 0, iter = 0;
+	while (counts < 10)
+	{
+		while (iter <= 14)
+		{
+			if (iter >= 10)
+				_putchar('1');
+			_putchar((iter % 10) + '0');
+			iter++;
+		}
+		_putchar('\n');
+		iter = 0;
+		counts++;
+	}
+}
