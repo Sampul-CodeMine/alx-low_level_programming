683875bd151b8fabaa3a348c1079c862f477743d
Sampul-CodeMine <studysampul@gmail.com>
2022-12-15 17:37:57 +0000

    A C program to print numbers preserving its values and its signs

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * main - Entry point to check codes
+  * Return: Always 0 (Success)
+  */
+
+int main(void)
+{
+	print_number(98);
+	_putchar('\n');
+	print_number(42);
+	_putchar('\n');
+	print_number(1024);
+	_putchar('\n');
+	print_number(0);
+	_putchar('\n');
+	print_number(-98);
+	_putchar('\n');
+	return (0);
+}
--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,17 +1,17 @@
-#include "holberton.h"
+#include "main.h"
 /**
  * print_number - prints a number preserving its signs
  * @num: the number to preserve and print
  * Return: Nothing
  */
 
-void print_number(int n)
+void print_number(int num)
 {
 	long len, res, i, temp, expo;
 
-	res = n;
+	res = num;
 	expo = len =  1;
-	
+
 	/*Check if the number is negative */
 	if (res < 0)
 	{
@@ -28,7 +28,7 @@ void print_number(int n)
 
 	for (i = 1; i < len; i++)
 		expo *= 10;
-	
+
 	while (expo > 1)
 	{
 		_putchar((res / expo) % 10 + '0');
