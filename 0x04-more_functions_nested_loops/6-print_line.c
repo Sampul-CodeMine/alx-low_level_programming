a8c90f906e5e567caf7b589c7d116e044dbc1c59
Sampul-CodeMine <studysampul@gmail.com>
2022-12-15 14:51:01 +0100

    C program to draw a kine according to the specified length.

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - function to draw lines as per n characters
+ * @num: Number of dots or line to draw
+ * Return: Nothing
+ */
+
+void print_line(int num)
+{
+	int itr;
+	if (num > 0)
+	{
+		for (itr = 0; itr < num; itr++)
+		{
+			_putchar('_');
+		}
+	}
+	_putchar('\n');
+}
