ec35509682ea370c405e33ddcfdf13b6fda3bc1d
Sampul-CodeMine <studysampul@gmail.com>
2022-12-15 16:02:52 +0000

    A C program to perform a fizz buzz test on numbers which are divisible by 3 and/or 5

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+void fizz_buzz(void);
+
+/**
+  * main - Entry Point
+  * Return: Nothing
+  */
+
+int main(void)
+{
+	fizz_buzz();
+	return (0);
+}
+
+/**
+  * fizz_buzz - function to swap values  with words according to conditions
+  * Return: nothing
+  */
+
+void fizz_buzz(void)
+{
+	int itr;
+
+	for (itr = 1; itr <= 100; itr++)
+	{
+		if (itr % 3 == 0 && itr % 5 == 0)
+			printf("FizzBuzz ");
+		else if (itr % 3 == 0)
+			printf("Fizz ");
+		else if (itr % 5 == 0)
+			printf("Buzz ");
+		else
+			printf("%d ", itr);
+	}
+}
