807a30fa2019ef2a30dced930554d249872879f8
Sampul-CodeMine <studysampul@gmail.com>
2022-12-19 15:46:01 +0100

    A C program to print a string in reverse order.

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,19 +8,16 @@
 
  void print_rev(char *str)
 {
-    char *full_string;
     int counter = 0, i;
 
 	while (*str != '\0')
 	{
-		full_string[counter] = putchar(*str);
 		*str++;
 		counter++;
 	}
-    printf("\nLength of the String: %d\n", counter);
-    printf("String: %s\n", full_string);
     for (counter -= 1; counter >= 0; counter--)
     {
-        putchar(full_string[counter]);
+        _putchar(str[counter]);
     }
+    _putchar('\n');
 }
