52c70dcddf1ecb19cd84bbf256c672eeba09b988
Sampul-CodeMine <studysampul@gmail.com>
2022-12-19 12:52:50 +0100

    C program to swap the values of 2 variablea between themselves.

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - function to swap values between two
+ * integer variables
+ * @num_one: pointer to address of first variable
+ * @num_two: pointer to address of second variable
+ * Return: Nothing
+ */
+
+void swap_int(int *num_one, int *num_two)
+{
+	int tmp;
+	tmp = *num_one;
+	*num_one = *num_two;
+	*num_two = tmp;
+}
