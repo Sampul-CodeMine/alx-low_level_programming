cc559c7d3aae694055ceca7faf0360ce5eba98d4
Sampul-CodeMine <studysampul@gmail.com>
2022-12-19 12:35:44 +0100

    C program to replace the value of a variable by dereferencing.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function to reset the vslue of a variable
+ * @num: a pointer to the address of the integer variable
+ * Return: Nothing
+ */
+
+void reset_to_98(int *num)
+{
+	*num = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+int _putchar(int);
+void reset_to_98(int *);
+
+#endif /* MAIN_H */
