413f4f114ccfb7a977b1af0e00085c9bf99370c1
Sampul-CodeMine <studysampul@gmail.com>
2022-12-19 20:50:52 +0100

    A C program to accept a string and print half of its length.

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - function to return half a string
+ * @str: string for which to retun hslf its size
+ * Return: Nothing.
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i=0;
+	while (str[i] != '\0')
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -9,5 +9,7 @@ void swap_int(int *, int *);
 int _strlen(char *);
 void _puts(char *);
 void print_rev(char *);
+void puts2(char *);
+
 
 #endif /* MAIN_H */
