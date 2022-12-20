bbeb1336fd7e8b17fac2ca78c554c2fcf5a9a803
Sampul-CodeMine <studysampul@gmail.com>
2022-12-20 10:39:49 +0000

    Password generator code in c programming for the 101-crackme file

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,5 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include <time.h>
 
 /**
@@ -9,39 +7,19 @@
 
 int main(void)
 {
-	char passwd[14];
-	int index = 0, total = 0, first_half, second_half;
+	int temp = 0, counter = 0;
+	time_t t;
 
-	srand(time(0));
-	while (total < 2772)
+	srand((unsigned int) time(&t));
+
+	while (counter < 2772)
 	{
-		passwd[index] = 33 + rand() % 94;
-		total += passwd[index++];
+		temp = rand() % 128;
+		if ((counter + temp) > 2772)
+			break;
+		counter += temp;
+		printf("%c", temp);
 	}
-	passwd[index] = '\0';
-	if (total != 2772)
-	{
-		first_half = (total - 2772) / 2;
-		second_half = (total - 2772) / 2;
-		if ((total - 2772) % 2 != 0)
-			first_half++;
-		for (index = 0; passwd[index]; index++)
-		{
-			if (passwd[index] >= (33 + first_half))
-			{
-				passwd[index] -= first_half;
-				break;
-			}
-		}
-		for (index = 0; passwd[index]; index++)
-		{
-			if (passwd[index] >= (33 + second_half))
-			{
-				passwd[index] -= second_half;
-				break;
-			}
-		}
-	}
-	printf("%s", passwd);
+	printf("%c\n", (2772 - counter));
 	return (0);
 }
