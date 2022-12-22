f1a56bf1e4ba9dafbabd484661117f6d63c62bc1
Sampul-CodeMine <studysampul@gmail.com>
2022-12-22 19:25:38 +0100

    C program to reverse the order of an array and print it out.

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,1 +1,26 @@
 #include "main.h"
+
+/**
+  * reverse_array - function to reverse the order of an array and print out
+  * @arr: an integer array
+  * @len: length of array to reverse
+  * Return: Nothing
+  */
+
+void reverse_array(int *arr, int len)
+{
+	int *ptr, itr, temp, x;
+
+	ptr = arr;
+	
+	for (itr = 1; itr < len; itr++)
+		ptr++;
+	
+	for (x = 0; x < (itr / 2); x++)
+	{
+		temp = arr[x];
+		a[x] = *ptr;
+		*ptr = temp;
+		ptr--;
+	}
+}
