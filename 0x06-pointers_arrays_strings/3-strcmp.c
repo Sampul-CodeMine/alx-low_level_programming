8eba4ff375522f07df2bf9e0e297eb37629a45f5
Sampul-CodeMine <studysampul@gmail.com>
2022-12-22 16:09:38 +0100

    A c program to compare two strings and return where the difference is

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,7 +11,7 @@ int _strcmp(char *source_one, char *source_two)
 {
         int itr, count = 0;
 
-        for (itr = 0; (source_two[itr] != '\0' && source_two[itr] != '\0') && source_two[itr] == source_two[itr]; itr++)
+        for (itr = 0; (source_two[itr] != '\0' && source_two[itr] != '\0') && source_one[itr] == source_two[itr]; itr++)
 		++count;
 
         return (source_one[count] - source_two[count]);
