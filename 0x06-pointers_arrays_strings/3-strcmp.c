c73c422cf42af2ff8c48271fd80240c1bc5375ef
Sampul-CodeMine <studysampul@gmail.com>
2022-12-22 17:20:48 +0100

    C program  to compare 2 strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,10 +9,9 @@
 
 int _strcmp(char *source_one, char *source_two)
 {
-        int itr, count = 0;
-
-        for (itr = 0; (source_two[itr] != '\0' && source_two[itr] != '\0') && source_one[itr] == source_two[itr]; itr++)
+	int itr, count = 0;
+	for (itr = 0; (source_two[itr] != '\0' && source_two[itr] != '\0') && source_one[itr] == source_two[itr]; itr++)
 		++count;
 
-        return (source_one[count] - source_two[count]);
+	return (source_one[count] - source_two[count]);
 }
