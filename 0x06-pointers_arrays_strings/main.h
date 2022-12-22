d3610d1ab749abf0dd81f2881164e41e59ed6204
Sampul-CodeMine <studysampul@gmail.com>
2022-12-22 17:30:48 +0100

    A c program to print an array in reverse form

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -9,5 +9,9 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int size);
 char *_strncpy(char *dest, char *src, int length);
 int _strcmp(char *, char *);
+void reverse_array(int *, int);
+
+
+
 
 #endif /* MAIN_H */
