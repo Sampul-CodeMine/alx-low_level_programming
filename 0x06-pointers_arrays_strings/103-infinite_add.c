f42e30261019f834c59ed3fd00b888a3a5b8888f
Sampul-CodeMine <studysampul@gmail.com>
2022-12-23 12:13:30 +0100

    A program to add numbers infinitely.

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -1,51 +1,147 @@
 #include "main.h"
 
 /**
- * infinite_add - Adds two numbers
- * @n1: pointer to first input string
- * @n2: pointer to second input string
- * @r: pointer to buffer memory where result is stored
- * @size_r: requested size for the buffer memory
- * Return: pointer to buffer memory where result is stored
+
+ * rev_string - reverse array
+
+ * @n: integer params
+
+ * Return: 0
+
  */
 
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
+void rev_string(char *n)
+
 {
-	int i, j, n, len1, len2; /* i = iterator, n = carry over number */
 
-	for (len1 = 0; n1[len1] != '\0'; len1++)
-		;
-	for (len2 = 0; n2[len2] != '\0'; len2++)
-		;
-	if (size_r >= len1 || size_r >= len2)
-		if (len1 >= len2)
-			i = j = len2 - 1;
-		else
-			i = j = len1 - 1;
-	else
-		i = j = size_r;
-	n = 0;
-	/* if first number >= 10, set the value to 1 and increase the buffer by 1.*/
-	if ((n1[0] - '0') + (n2[0] - '0') >= 10)
-	{
-		r[0] = 1 + '0';
-		j = 1;
-	}
+        int i = 0;
 
-	while (i >= 0)
-	{
-		r[j] = (n1[i] - '0') + (n2[i] - '0') + n;
-		if (r[j] >= 10)
-		{
-			r[j] %= 10;
-			n = 1;
-		}
-		else
-			n = 0;
-		r[j] += '0';
-		i--;
-		j--;
-	}
-	r[size_r] = '\0';
-	return (r);
+        int j = 0;
+
+        char temp;
+
+
+        while (*(n + i) != '\0')
+
+        {
+
+                i++;
+
+        }
+
+        i--;
+
+
+        for (j = 0; j < i; j++, i--)
+
+        {
+
+                temp = *(n + j);
+
+                *(n + j) = *(n + i);
+
+                *(n + i) = temp;
+
+        }
+
+}
+
+
+/**
+
+ * infinite_add - add 2 numbers together
+
+ * @n1: text representation of 1st number to add
+
+ * @n2: text representation of 2nd number to add
+
+ * @r: pointer to buffer
+
+ * @size_r: buffer size
+
+ * Return: pointer to calling function
+
+ */
+
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r)
+
+{
+
+        int overflow = 0, i = 0, j = 0, digits = 0;
+
+        int val1 = 0, val2 = 0, temp_tot = 0;
+
+
+        while (*(n1 + i) != '\0')
+
+                i++;
+
+        while (*(n2 + j) != '\0')
+
+                j++;
+
+        i--;
+
+        j--;
+
+        if (j >= size_r || i >= size_r)
+
+                return (0);
+
+        while (j >= 0 || i >= 0 || overflow == 1)
+
+        {
+
+                if (i < 0)
+
+                        val1 = 0;
+
+                else
+
+                        val1 = *(n1 + i) - '0';
+
+                if (j < 0)
+
+                        val2 = 0;
+
+                else
+
+                        val2 = *(n2 + j) - '0';
+
+                temp_tot = val1 + val2 + overflow;
+
+                if (temp_tot >= 10)
+
+                        overflow = 1;
+
+                else
+
+                        overflow = 0;
+
+                if (digits >= (size_r - 1))
+
+                        return (0);
+
+                *(r + digits) = (temp_tot % 10) + '0';
+
+                digits++;
+
+                j--;
+
+                i--;
+
+        }
+
+        if (digits == size_r)
+
+                return (0);
+
+        *(r + digits) = '\0';
+
+        rev_string(r);
+
+        return (r);
+
 }
