29b09ced8aeed6865c58c56d43a51babbb9b1369
Sampul-CodeMine <studysampul@gmail.com>
2022-12-23 11:29:51 +0100

    A C program to perform magic.

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 int main(void)
 {
@@ -19,5 +19,6 @@ int main(void)
 	*(p + 5) = 98;
 	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
+
 	return (0);
 }
