8ed3537348ac46f20feee5746e1944e9a75f88a6
Sampul-CodeMine <studysampul@gmail.com>
2023-01-06 13:57:18 +0100

    Program to sum up numbers infinitely

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,43 @@
+#include "main.h"
+
+/**
+ * main - Program entry point: find the sum of numbers infinitely.
+ * @argc: counts the numner of parameters passed to program in the CLI
+ * @argv: pointer to an array of parameters passed to the program in the CLI
+ * Return: an integer
+ */
+
+int main(int argc, char *argv[])
+{
+        int counter;
+        unsigned int iter, total = 0;
+        char *current;
+
+        if (argc > 1)
+        {
+                for (counter = 1; counter < argc; counter++)
+                {
+                        current = argv[counter];
+                        for (iter = 0; iter < strlen(current); iter++)
+                        {
+                                if (current[iter] >= 48 && current[iter] <= 57)
+                                {
+                                        total += atoi(current);
+                                        current++;
+                                }
+                                else
+                                {
+                                        printf("Error\n");
+                                        return (1);
+                                }
+                        }
+                }
+                printf("%d\n", total);
+        }
+        else
+        {
+                printf("0\n");
+        }
+
+        return (0);
+}
