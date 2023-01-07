d5ca6ffbf0a89791836b78c646f50693b9035582
Sampul-CodeMine <studysampul@gmail.com>
2023-01-07 02:22:05 +0100

    Created a C library

---
+++ 0x09-static_libraries/README.md
@@ -0,0 +1,2 @@
+This is a readme file on how to make C libraries and archives.
+Static and Dynamic libraries.
---
+++ 0x09-static_libraries/main.h
@@ -0,0 +1,28 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int _putchar(int);
+int _islower(int);
+int _isdigit(int);
+int _isalpha(int);
+int _abs(int);
+int _strlen(char *);
+int _atoi(char *);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int size);
+char *_strncpy(char *dest, char *src, int length);
+int _strcmp(char *, char *);
+int _isupper(int);
+void _puts(char *);
+char *_strcpy(char *, char *);
+char *_memset(char *, char, unsigned int);
+char *_memcpy(char *, char *, unsigned int);
+char *_strchr(char *, char);
+unsigned int _strspn(char *, char *);
+char *_strpbrk(char *, char *);
+char *_strstr(char *, char *);
+
+#endif /* MAIN_H */
