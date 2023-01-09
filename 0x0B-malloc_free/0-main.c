f55e6017400c523e7257eb3b3a852ba2c4473ed2
Sampul-CodeMine <studysampul@gmail.com>
2023-01-09 16:51:36 +0100

    Hot fix

--- 0x0B-malloc_free/0-main.c
+++ 0x0B-malloc_free/0-main.c
@@ -35,15 +35,15 @@ void simple_print_buffer(char *buffer, unsigned int size)
 
 int main(void)
 {
-        char *buffer;
-        
-        buffer = create_array(98, 'H');
-        if  (buffer == NULL)
-        {
-                printf("failed to allocate memory\n");
-                return (1);
-        }
-        simple_print_buffer(buffer, 98);
-        free(buffer);
-        return (0);
+	char *buffer;
+
+	buffer = create_array(98, 'H');
+	if  (buffer == NULL)
+	{
+		printf("failed to allocate memory\n");
+		return (1);
+	}
+	simple_print_buffer(buffer, 98);
+	free(buffer);
+	return (0);
 }
