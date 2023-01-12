72ee636f539e4000ae605df7f1e0503c640b5e2c
Sampul-CodeMine <studysampul@gmail.com>
2023-01-12 16:14:13 +0100

    A c program to initialize an array's elements to 0

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _calloc - function that allocates memory to an array using malloc
+ * @nmemb: number of memory for elements in the array
+ * @size: the size of array elements
+ * Return: NULL if fails or else returns the array pointer
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	unsigned int itr;
+	char *ptr;
+
+	itr = 0;
+
+	if (nmemb == 0 || size == 0)
+		return (0);
+
+	ptr = malloc(nmemb * size);
+
+	if (ptr == 0)
+		return (0);
+	for (itr = 0; itr < size; itr++)
+		ptr[itr] = 0;
+
+	return (ptr);
+}
