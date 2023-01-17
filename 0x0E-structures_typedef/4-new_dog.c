9cfa4a5e2f6ad096864d07dda55bf84ca08b3aac
Sampul-CodeMine <studysampul@gmail.com>
2023-01-17 08:51:36 +0100

    New dog struct

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,78 +1,48 @@
 #include "dog.h"
 
 /**
-  * strcpy - Copy a string from source to destination
-  * @dest: Destination value
-  * @src: Source value
-  * Return: the pointer to dest
+  * new_dog - new dog
+  * @name: argument for name of dog
+  * @age: argument for age of dog
+  * @owner: argument for owner of dog
+  * Return: newdog
   */
 
-char *str_cpy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; src[i] != '\0'; i++)
-		dest[i] = src[i];
-	dest[i++] = '\0';
-	return (dest);
-}
-
-
-/**
-  * strlen - Returns the length of a string
-  * @s: String to count
-  * Return: String length
-  */
-
-int str_len(char *s)
-{
-	int c = 0;
-
-	for (; *s != '\0'; s++)
-		c++;
-	return (c);
-}
-
-/**
-  * new_dog - ...
-  * @name: ...
-  * @age: ...
-  * @owner: ...
-  *
-  * Return: ...
-  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *n_dog;
-	int name_l = 0, own_l = 0;
 
-	name_l = str_len(name) + 1;
-	own_l = str_len(owner) + 1;
-	n_dog = malloc(sizeof(dog_t));
+	int nmlen = 0, ownrlen = 0, itr;
+	dog_t *bingo;
 
-	if (n_dog == NULL)
-		return (NULL);
-
-	n_dog->name = malloc(sizeof(char) * name_l);
-
-	if (n_dog->name == NULL)
+	while (name[nmlen] != '\0')
+		nmlen++;
+	while (owner[ownrlen] != '\0')
+		ownrlen++;
+	bingo = malloc(sizeof(dog_t));
+	if (bingo == NULL)
 	{
-		free(n_dog);
+		free(bingo);
 		return (NULL);
 	}
-
-	n_dog->owner = malloc(sizeof(char) * own_l);
-
-	if (n_dog->owner == NULL)
+	bingo->name = malloc(nmlen * sizeof(bingo->name));
+	if (bingo->name == NULL)
 	{
-		free(n_dog->name);
-		free(n_dog);
+		free(bingo->name);
+		free(bingo);
 		return (NULL);
 	}
-
-	n_dog->name = str_cpy(n_dog->name, name);
-	n_dog->owner = str_cpy(n_dog->owner, owner);
-	n_dog->age = age;
-
-	return (n_dog);
+	for (itr = 0; itr <= nmlen; itr++)
+		bingo->name[itr] = name[itr];
+	bingo->age = age;
+	bingo->owner = malloc(ownrlen * sizeof(bingo->owner));
+	if (bingo->owner == NULL)
+	{
+		free(bingo->owner);
+		free(bingo->name);
+		free(bingo);
+		return (NULL);
+	}
+	for (itr = 0; itr <= ownrlen; itr++)
+		bingo->owner[itr] = owner[itr];
+	return (bingo);
 }
