2510c8f4c4aebd1fb989ef44906c99139c90c4e6
--help <studysampul@gmail.com>
2023-01-26 16:05:34 +0100

    This is the solution to printing the length of a list.

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,9 @@
 #include "lists.h"
 
 /**
+ * list_len - A function to print the length of a list
+ * @head: pointer to the next node in the list
+ * Return: the length of the list
  */
 
 size_t list_len(const list_t *head)
