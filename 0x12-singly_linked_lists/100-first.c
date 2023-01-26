feb4e7ace00411f7f9d40764c4b7dbe7840addbf
Sampul-CodeMine <studysampul@gmail.com>
2023-01-26 11:23:48 -0800

    Advance Task fix

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,12 +3,13 @@
 void init_loader(void) __attribute__((constructor));
 
 /**
- * init_loader - this is a function that acts as a constructor 
+ * init_loader - this is a function that acts as a constructor
  * and it runs before the main function loads.
  * Return: Nothing.
  */
 
 void init_loader(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,");
+	printf("\nI bore my house upon my back!\n");
 }
