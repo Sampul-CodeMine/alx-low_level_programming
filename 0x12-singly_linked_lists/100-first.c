bd3711defa7e6ddce0e80c86a820018fcc532bb1
--help <studysampul@gmail.com>
2023-01-26 20:16:50 +0100

    Advanced tasks

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void init_loader(void) __attribute__((constructor));
+
+/**
+ * init_loader - this is a function that acts as a constructor 
+ * and it runs before the main function loads.
+ * Return: Nothing.
+ */
+
+void init_loader(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
