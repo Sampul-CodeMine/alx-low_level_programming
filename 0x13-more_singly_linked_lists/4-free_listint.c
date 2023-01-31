e2dd5e833fe289a67dd10a8e497fc4f990ef3163
Dukeson Ehigboria O <studysampul@gmail.com>
2023-01-31 09:04:29 +0100

    Updated my codes

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -13,6 +13,5 @@ void free_listint(listint_t *head)
 		return;
 
 	free_listint(head->next);
-	free(head->str);
 	free(head);
 }
--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,21 +1,22 @@
 #include "lists.h"
-#include <stdlib.h>
+
 /**
   * free_listint2 - Frees a list and sets its head to NULL
   * @head: double pointer to head of list
   */
+
 void free_listint2(listint_t **head)
 {
-	listint_t *temp, *current;
+	listint_t *temp_loc, *current_loc;
 
 	if (head == NULL)
 		return;
-	current = *head;
-	while (current != NULL)
+	current_loc = *head;
+	while (current_loc != NULL)
 	{
-		temp = current;
-		current = current->next;
-		free(temp);
+		temp_loc = current_loc;
+		current_loc = current_loc->next;
+		free(temp_loc);
 	}
 	*head = NULL;
 }
