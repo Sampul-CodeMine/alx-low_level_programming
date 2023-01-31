501eb0aea268f59165e048e05960abcbc735f775
Dukeson Ehigboria O <studysampul@gmail.com>
2023-01-31 16:44:38 +0100

    Completed Codes

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,5 +1,7 @@
 #include "lists.h"
 
+size_t list_looping(const listint_t *head);
+
 /**
  * print_listint_safe - prints a singly linked list
  * (can print lists with loop)
@@ -9,27 +11,67 @@
 
 size_t print_listint_safe(const listint_t *head)
 {
-	const listint_t *temp_loc, *temp_loc2;
-	unsigned int itr = 0;
+	size_t list_nodes, node_index;
 
-	temp_loc = head;
-	if (temp_loc == 0)
-		return (0);
-
-	while (temp_loc != 0)
+	node_index = 0;
+	list_nodes = list_looping(head);
+	if (list_nodes == 0)
 	{
-		temp_loc2 = temp_loc;
-		temp_loc = temp_loc->next;
-
-		itr++;
-
-		printf("[%p] %d\n", (void *)temp_loc2, temp_loc2->n);
-
-		if (temp_loc2 <= temp_loc)
+		for (list_nodes = 0; head != NULL; list_nodes++)
 		{
-			printf("-> [%p] %d\n", (void *)temp_loc, temp_loc->n);
-			break;
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
 		}
 	}
-	return (itr);
+	else
+	{
+		for (node_index = 0; node_index < list_nodes; node_index++)
+		{
+			printf("[%p] %d\n", (void *)head, head->n);
+			head = head->next;
+		}
+		printf("-> [%p] %d\n", (void *)head, head->n);
+	}
+	return (list_nodes);
+}
+
+/**
+ * list_looping - function to loop through a list's nodes
+ * @head: pointer to the singly linked list 
+ * Return: 0 if not looped else return the number of unique nodes in the list
+ */
+
+size_t list_looping(const listint_t *head)
+{
+	const listint_t *slow, *fast;
+	size_t found = 1;
+
+	if (head == NULL || head->next == NULL)
+		return (0);
+	
+	slow = head->next;
+	fast = (head->next)->next;
+	while (fast)
+	{
+		if (slow == fast)
+		{
+			slow = head;
+			while (slow != fast)
+			{
+				found++;
+				slow = slow->next;
+				fast = fast->next;
+			}
+			slow = slow->next;
+			while (slow != fast)
+			{
+				found++;
+				slow = slow->next;
+			}
+			return (found);
+		}
+		slow = slow->next;
+		fast = (fast->next)->next;
+	}
+	return (0);
 }
