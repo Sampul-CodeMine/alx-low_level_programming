bb4e4c3b5dea4f035e9a81a846f9de492096cef1
Dukeson Ehigboria O <studysampul@gmail.com>
2023-02-06 17:04:48 +0100

    Solutions to Task for File I/O

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+
+int _putchar(char);
+ssize_t read_textfile(const char *, size_t);
+
+#endif /* MAIN_H */
