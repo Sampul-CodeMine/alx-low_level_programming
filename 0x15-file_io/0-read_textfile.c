389878509a6c49a1138235426217d7741f27e2f6
Dukeson Ehigboria O <studysampul@gmail.com>
2023-02-06 17:27:29 +0100

    Solution to task one

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - function to read the content of a textfile and print out to STDOUT
+ * @ile_name: fullpath of the file to be read 
+ * @chrs: characters to read.
+ * Return: on success return 0
+ */
+ssize_t read_textfile(const char *file_name, size_t chrs)
+{
+	int filedesc;
+	int read_chars;
+	char *buffer;
+
+	/* sets the size of the buffer */
+	buffer = malloc(sizeof(char *) * chrs);
+
+	/* if the buffer is empty*/
+	if (buffer == NULL)
+		return (0);
+
+	/* if the filename is empty*/
+	if (file_name == NULL)
+		return (0);
+
+	/* set the file descriptor */
+	filedesc = open(file_name, O_RDONLY);
+	if (filedesc == -1)
+		return (0);
+
+	read_chars = read(filedesc, buffer, chrs);
+	write(STDOUT_FILENO, buffer, read_chars);
+
+	free(buffer);
+	close(filedesc);
+	return (read_chars);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -7,5 +7,7 @@
 
 int _putchar(char);
 ssize_t read_textfile(const char *, size_t);
+int create_file(const char *, char *);
+int append_text_to_file(const char *, char *);
 
 #endif /* MAIN_H */
