f15889baa9afa15349278edad885821a5ebb64f4
Dukeson Ehigboria O <studysampul@gmail.com>
2023-03-16 19:08:01 +0100

    Solution to Task 1

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * struct dlistint_s - doubly linked list
@@ -22,6 +23,6 @@ typedef struct dlistint_s
 } dlistint_t;
 
 size_t print_dlistint(const dlistint_t *h);
-
+size_t dlistint_len(const dlistint_t *h);
 
 #endif /* LIST_H */
