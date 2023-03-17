f66e7caee857751cbfe8e4777eaf3ed72fd887b5
Dukeson Ehigboria O <studysampul@gmail.com>
2023-03-17 17:11:45 +0100

    Solution to Task 0 - Doubly-linked list

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -3,18 +3,17 @@
 /**
  * print_dlistint - prints all the elements of a dlistint_t list.
  * @h: head of doubly linked list
- *
  * Return: the number of nodes
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
-	while (h != NULL)
+	for (count = 0; h != NULL; count++)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		count++;
 	}
 	return (count);
 }
