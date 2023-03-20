02003803a6e3055fd8afb7a5c46057c742fb0886
Sampul-CodeMine <studysampul@gmail.com>
2023-03-20 18:32:45 +0100

    Updates on Task 0

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -19,7 +19,7 @@ char *_strcat(char *, char *);
 char *_strncat(char *, char *, int);
 char *_strncpy(char *, char *, int);
 int _strcmp(char *, char *);
-char *_memset(char *, char, unsigned int );
+char *_memset(char *, char, unsigned int);
 char *_memcpy(char *, char *, unsigned int);
 char *_strchr(char *, char);
 unsigned int _strspn(char *, char *);
