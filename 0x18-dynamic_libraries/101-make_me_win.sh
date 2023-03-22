727d9dad7eea45a9353c07dfdaaa5706f248b0f6
Sampul-CodeMine <studysampul@gmail.com>
2023-03-22 09:34:14 -0700

    Updates on the task 4

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/sidneyriffic/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libcracker.so
+wget -P /tmp https://github.com/Sampul-CodeMine/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libcracker.so
 export LD_PRELOAD=/tmp/libcracker.so
