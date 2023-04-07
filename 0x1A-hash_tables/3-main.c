e0e70f18f25ad5151cc86c68bea9dc48fe2ae3b8
Dukeson Ehigboria O <studysampul@gmail.com>
2023-04-07 15:48:51 +0100

    Solution to Task 3

--- 0x1A-hash_tables/3-main.c
+++ 0x1A-hash_tables/3-main.c
@@ -11,5 +11,12 @@ int main(void)
 
 	ht = hash_table_create(1024);
 	hash_table_set(ht, "betty", "cool");
+	hash_table_set(ht, "alx", "hetairas");
+	hash_table_set(ht, "holberton", "mentioner");
+	hash_table_set(ht, "kimba", "heliotropes");
+	hash_table_set(ht, "sky", "neurospora");
+	hash_table_set(ht, "sololearn", "depravement");
+	hash_table_set(ht, "udemy", "serafins");
+
 	return (EXIT_SUCCESS);
 }
