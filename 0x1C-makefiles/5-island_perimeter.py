f2ff8c049d1376d663711a9b8b1c08fc50b725d0
Dukeson Ehigboria O <studysampul@gmail.com>
2023-05-25 13:30:41 +0100

    Update 5-island_perimeter.py

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,22 +8,17 @@ def island_perimeter(grid):
     grid: (list of lists) the islands geographical landscape
     Returns: Perimeter of the island
     """
-    itr = 0
-    row = len(grid)
-    col = len(grid[0]) if row else 0
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    sizes = 0
 
-    for i in range(len(grid)):
-        for j in range(len(grid[i])):
-
-            mark = [(i - 1, j), (i, j - 1), (i, j + 1), (i + 1, j)]
-            check = []
-            for k in mark:
-                if k[0] in range(row) and k[1] in range(col):
-                    check.append(1)
-                else:
-                    check.append(1)
-            if grid[i][j]:
-                itr += sum([1 if not r or not grid[k[0]][k[1]] else 0
-                            for r, k in zip(check, mark)])
-
-    return (itr)
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] == 1:
+                sizes += 1
+                if (j > 0 and grid[i][j - 1] == 1):
+                    edges += 1
+                if (i > 0 and grid[i - 1][j] == 1):
+                    edges += 1
+    return sizes * 4 - edges * 2
