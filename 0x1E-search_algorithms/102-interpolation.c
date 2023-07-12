103f900702a23ffdfd053b6c161b4ed329f5f0b2
Sampul-CodeMine <studysampul@gmail.com>
2023-07-12 17:54:58 +0100

    Updated Task 102

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -36,7 +36,7 @@ int interpolation_search(int *array, size_t size, int value)
 
 	for (low = 0, high = (size - 1); high >= low;)
 	{
-		pos = low + (((double)(high - low) / (array[high] - array[low])) * (value - array[low]));
+		pos = get_position(array, value, low, high);
 		if (pos < size)
 			printf("Value checked array[%ld] = [%d]\n", pos, array[pos]);
 		else
